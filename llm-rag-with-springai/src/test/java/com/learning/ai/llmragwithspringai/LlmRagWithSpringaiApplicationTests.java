593712b8cb37901992826a65d1005675128e2154
Raja Kolli <rajadilipkolli@gmail.com>
2024-03-27 17:40:24 +0000

    feat : initial commit of llm-rag with spring AI

---
+++ llm-rag-with-springai/src/test/java/com/learning/ai/llmragwithspringai/LlmRagWithSpringaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.ai.llmragwithspringai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmRagWithSpringaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ llm-rag-with-springai/src/test/java/com/learning/ai/llmragwithspringai/TestLlmRagWithSpringaiApplication.java
@@ -0,0 +1,23 @@
+package com.learning.ai.llmragwithspringai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestLlmRagWithSpringaiApplication {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> pgvectorContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("pgvector/pgvector:pg16"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(LlmRagWithSpringaiApplication::main).with(TestLlmRagWithSpringaiApplication.class).run(args);
+	}
+
+}
