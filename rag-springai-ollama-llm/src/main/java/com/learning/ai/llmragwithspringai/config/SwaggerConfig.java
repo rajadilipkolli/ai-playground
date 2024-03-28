7bc76b5c7b209b9e0ac1ee14e0a40b5e06687cea
Raja Kolli <rajadilipkolli@gmail.com>
2024-03-28 09:06:48 +0000

    feat : removes unnecessary configuration

--- rag-springai-ollama-llm/src/main/java/com/learning/ai/llmragwithspringai/config/SwaggerConfig.java
+++ rag-springai-ollama-llm/src/main/java/com/learning/ai/llmragwithspringai/config/SwaggerConfig.java
@@ -6,5 +6,5 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
-@OpenAPIDefinition(info = @Info(title = "llm-rag-with-springai", version = "v1.0.0"), servers = @Server(url = "/"))
+@OpenAPIDefinition(info = @Info(title = "rag-springai-ollama-llm", version = "v1.0.0"), servers = @Server(url = "/"))
 public class SwaggerConfig {}
