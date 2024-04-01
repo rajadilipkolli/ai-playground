3b3f09e4e586f0df2f89d774810b277ccbe02477
Raja Kolli <rajadilipkolli@gmail.com>
2024-04-01 11:31:53 +0000

    feat : starts container without authentication

--- embeddingstores/neo4j-springai/src/test/java/com/learning/ai/TestNeo4jVectorEmbeddingStoreExample.java
+++ embeddingstores/neo4j-springai/src/test/java/com/learning/ai/TestNeo4jVectorEmbeddingStoreExample.java
@@ -13,7 +13,9 @@ public class TestNeo4jVectorEmbeddingStoreExample {
     @Bean
     @ServiceConnection
     Neo4jContainer<?> neo4jContainer() {
-        return new Neo4jContainer<>(DockerImageName.parse("neo4j:5"));
+        return new Neo4jContainer<>(DockerImageName.parse("neo4j:5"))
+                .withoutAuthentication()
+                .withLabsPlugins("apoc");
     }
 
     public static void main(String[] args) {
