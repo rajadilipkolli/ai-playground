dc63c5958ec97379b48b40d805455239ef9f7528
Raja Kolli <rajadilipkolli@gmail.com>
2024-04-07 09:24:41 +0000

    feat : implement simple RAG

--- chatmodel-springai/src/main/java/com/example/ai/controller/ChatController.java
+++ chatmodel-springai/src/main/java/com/example/ai/controller/ChatController.java
@@ -3,17 +3,8 @@ package com.example.ai.controller;
 import com.example.ai.model.request.AIChatRequest;
 import com.example.ai.model.response.AIChatResponse;
 import com.example.ai.model.response.ActorsFilms;
-import java.util.List;
-import java.util.Map;
-import org.springframework.ai.chat.ChatClient;
-import org.springframework.ai.chat.ChatResponse;
-import org.springframework.ai.chat.Generation;
-import org.springframework.ai.chat.messages.SystemMessage;
-import org.springframework.ai.chat.messages.UserMessage;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.chat.prompt.PromptTemplate;
-import org.springframework.ai.embedding.EmbeddingClient;
-import org.springframework.ai.parser.BeanOutputParser;
+import com.example.ai.service.ChatService;
+import java.io.IOException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,61 +16,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/ai")
 public class ChatController {
 
-    private final ChatClient chatClient;
+    private final ChatService chatService;
 
-    private final EmbeddingClient embeddingClient;
-
-    ChatController(ChatClient chatClient, EmbeddingClient embeddingClient) {
-        this.chatClient = chatClient;
-        this.embeddingClient = embeddingClient;
+    ChatController(ChatService chatService) {
+        this.chatService = chatService;
     }
 
     @PostMapping("/chat")
     AIChatResponse chat(@RequestBody AIChatRequest aiChatRequest) {
-        var answer = chatClient.call(aiChatRequest.query());
-        return new AIChatResponse(answer);
+        return chatService.chat(aiChatRequest.query());
     }
 
     @PostMapping("/chat-with-prompt")
     AIChatResponse chatWithPrompt(@RequestBody AIChatRequest aiChatRequest) {
-        PromptTemplate promptTemplate = new PromptTemplate("Tell me a joke about {subject}");
-        Prompt prompt = promptTemplate.create(Map.of("subject", aiChatRequest.query()));
-        ChatResponse response = chatClient.call(prompt);
-        Generation generation = response.getResult();
-        String answer = (generation != null) ? generation.getOutput().getContent() : "";
-        return new AIChatResponse(answer);
+        return chatService.chatWithPrompt(aiChatRequest.query());
     }
 
     @PostMapping("/chat-with-system-prompt")
     AIChatResponse chatWithSystemPrompt(@RequestBody AIChatRequest aiChatRequest) {
-        SystemMessage systemMessage = new SystemMessage("You are a sarcastic and funny chatbot");
-        UserMessage userMessage = new UserMessage("Tell me a joke about " + aiChatRequest.query());
-        Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
-        ChatResponse response = chatClient.call(prompt);
-        String answer = response.getResult().getOutput().getContent();
-        return new AIChatResponse(answer);
+        return chatService.chatWithSystemPrompt(aiChatRequest.query());
     }
 
     @PostMapping("/emebedding-client-conversion")
     AIChatResponse chatWithEmbeddingClient(@RequestBody AIChatRequest aiChatRequest) {
-        List<Double> embed = embeddingClient.embed(aiChatRequest.query());
-        return new AIChatResponse(embed.toString());
+        return chatService.getEmbeddings(aiChatRequest.query());
     }
 
     @GetMapping("/output")
     public ActorsFilms generate(@RequestParam(value = "actor", defaultValue = "Jr NTR") String actor) {
-        BeanOutputParser<ActorsFilms> outputParser = new BeanOutputParser<>(ActorsFilms.class);
+        return chatService.generateAsBean(actor);
+    }
 
-        String format = outputParser.getFormat();
-        String template = """
-				Generate the filmography for the actor {actor}.
-				{format}
-				""";
-        PromptTemplate promptTemplate = new PromptTemplate(template, Map.of("actor", actor, "format", format));
-        Prompt prompt = new Prompt(promptTemplate.createMessage());
-        ChatResponse response = chatClient.call(prompt);
-        Generation generation = response.getResult();
-
-        return outputParser.parse(generation.getOutput().getContent());
+    @PostMapping("/rag")
+    AIChatResponse chatUsingRag(@RequestBody AIChatRequest aiChatRequest) throws IOException {
+        return chatService.ragGenerate(aiChatRequest.query());
     }
 }
