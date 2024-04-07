9a15c49073707b18d6c75e0f49279d3c631cd59c
Raja Kolli <rajadilipkolli@gmail.com>
2024-04-07 15:00:19 +0000

    feat : set the system message

--- rag/rag-springai-ollama-llm/src/main/java/com/learning/ai/llmragwithspringai/service/AIChatService.java
+++ rag/rag-springai-ollama-llm/src/main/java/com/learning/ai/llmragwithspringai/service/AIChatService.java
@@ -13,6 +13,7 @@ import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Service;
 
@@ -23,18 +24,9 @@ public class AIChatService {
 
     private static final String template =
             """
-            You're assisting with questions about cricket
-
-            Cricket is a bat-and-ball game that is played between two teams of eleven players on a field at the centre of which is a 22-yard (20-metre) pitch with a wicket at each end,
-            each comprising two bails balanced on three stumps.
-            Two players from the batting team (the striker and nonstriker) stand in front of either wicket,
-            with one player from the fielding team (the bowler) bowling the ball towards the striker's wicket from the opposite end of the pitch.
-            The striker's goal is to hit the bowled ball and then switch places with the nonstriker,
-            with the batting team scoring one run for each exchange.
-            Runs are also scored when the ball reaches or crosses the boundary of the field or when the ball is bowled illegally.
-
-            Use the information from the DOCUMENTS section to provide accurate answers but act as if you knew this information innately.
-            If unsure, simply state that you don't know.
+            You are a helpful assistant, conversing with a user about the subjects contained in a set of documents.
+            Use the information from the DOCUMENTS section to provide accurate answers. If unsure or if the answer
+            isn't found in the DOCUMENTS section, simply state that you don't know the answer.
 
             DOCUMENTS:
             {documents}
@@ -52,7 +44,8 @@ public class AIChatService {
     public String chat(String query) {
         // Querying the VectorStore using natural language looking for the information about info asked.
         LOGGER.info("Querying vector store with query :{}", query);
-        List<Document> listOfSimilarDocuments = this.vectorStore.similaritySearch(query);
+        List<Document> listOfSimilarDocuments = this.vectorStore.similaritySearch(
+                SearchRequest.query(query).withTopK(2).withSimilarityThreshold(0.8d));
         String documents = listOfSimilarDocuments.stream()
                 .map(Document::getContent)
                 .collect(Collectors.joining(System.lineSeparator()));
