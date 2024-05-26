a89d6e406e7860616b3c94ff514ef120164273b6
Raja Kolli <rajadilipkolli@gmail.com>
2024-05-26 11:09:20 +0000

    feat : adds vector store to chatbot

--- chatbot-ollama-springai/src/main/java/com/example/chatbot/service/ChatbotService.java
+++ chatbot-ollama-springai/src/main/java/com/example/chatbot/service/ChatbotService.java
@@ -1,16 +1,11 @@
 package com.example.chatbot.service;
 
 import com.example.chatbot.model.response.AIChatResponse;
-import java.util.List;
-import org.springframework.ai.chat.memory.*;
 import org.springframework.ai.chat.messages.UserMessage;
-import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.transformer.ChatServiceContext;
 import org.springframework.ai.chat.service.ChatService;
 import org.springframework.ai.chat.service.ChatServiceResponse;
-import org.springframework.ai.chat.service.PromptTransformingChatService;
-import org.springframework.ai.tokenizer.TokenCountEstimator;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,13 +13,8 @@ public class ChatbotService {
 
     private final ChatService chatService;
 
-    ChatbotService(ChatModel chatModel, ChatMemory chatMemory, TokenCountEstimator tokenCountEstimator) {
-        this.chatService = PromptTransformingChatService.builder(chatModel)
-                .withRetrievers(List.of(new ChatMemoryRetriever(chatMemory)))
-                .withContentPostProcessors(List.of(new LastMaxTokenSizeContentTransformer(tokenCountEstimator, 1000)))
-                .withAugmentors(List.of(new SystemPromptChatMemoryAugmentor()))
-                .withChatServiceListeners(List.of(new ChatMemoryChatServiceListener(chatMemory)))
-                .build();
+    public ChatbotService(ChatService chatService) {
+        this.chatService = chatService;
     }
 
     public AIChatResponse chat(String message) {
