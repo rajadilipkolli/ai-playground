cb42510d3149b3193d8c60dbc4591c66f2d808d0
rajadilipkolli <rajadilipkolli@gmail.com>
2024-05-30 12:25:58 +0530

    fix : update use case

--- chatbot/chatbot-ollama-springai/src/main/java/com/example/chatbot/service/ChatbotService.java
+++ chatbot/chatbot-ollama-springai/src/main/java/com/example/chatbot/service/ChatbotService.java
@@ -1,14 +1,13 @@
 package com.example.chatbot.service;
 
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
+
 import com.example.chatbot.model.request.AIChatRequest;
 import com.example.chatbot.model.response.AIChatResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.ai.chat.messages.UserMessage;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.chat.prompt.transformer.ChatServiceContext;
-import org.springframework.ai.chat.service.ChatService;
-import org.springframework.ai.chat.service.ChatServiceResponse;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,19 +15,22 @@ public class ChatbotService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ChatbotService.class);
 
-    private final ChatService chatService;
+    private final ChatClient chatClient;
 
-    public ChatbotService(ChatService chatService) {
-        this.chatService = chatService;
+    public ChatbotService(ChatClient chatClient) {
+        this.chatClient = chatClient;
     }
 
     public AIChatResponse chat(AIChatRequest request) {
-        Prompt prompt = new Prompt(new UserMessage(request.query()));
         String conversationId = request.conversationId() == null ? "default" : request.conversationId();
-        ChatServiceResponse chatServiceResponse = this.chatService.call(new ChatServiceContext(prompt, conversationId));
-        LOGGER.info("Response :{}", chatServiceResponse.getChatResponse().getResult());
-        return new AIChatResponse(
-                chatServiceResponse.getChatResponse().getResult().getOutput().getContent(),
-                chatServiceResponse.getPromptContext().getConversationId());
+
+        ChatClient.ChatClientRequest.CallResponseSpec call = this.chatClient
+                .prompt()
+                .user(request.query())
+                .advisors(a -> a.param(CHAT_MEMORY_CONVERSATION_ID_KEY, conversationId))
+                .call();
+        ChatResponse chatResponse = call.chatResponse();
+        LOGGER.info("Response :{}", chatResponse.getResult());
+        return new AIChatResponse(chatResponse.getResult().getOutput().getContent(), null);
     }
 }
