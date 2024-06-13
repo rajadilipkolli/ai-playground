013d514ca427f02daf0f755715b6038ebb8883bd
Raja Kolli <rajadilipkolli@gmail.com>
2024-06-13 15:23:08 +0000

    feat : fix compilation issue

--- chatbot/chatbot-openai-springai/src/main/java/com/example/chatbot/config/ChatConfig.java
+++ chatbot/chatbot-openai-springai/src/main/java/com/example/chatbot/config/ChatConfig.java
@@ -1,17 +1,13 @@
 package com.example.chatbot.config;
 
-import java.util.List;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.client.advisor.VectorStoreChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.memory.ChatMemoryChatServiceListener;
-import org.springframework.ai.chat.memory.ChatMemoryRetriever;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
-import org.springframework.ai.chat.memory.LastMaxTokenSizeContentTransformer;
-import org.springframework.ai.chat.memory.SystemPromptChatMemoryAugmentor;
-import org.springframework.ai.chat.model.ChatModel;
-import org.springframework.ai.chat.service.ChatService;
-import org.springframework.ai.chat.service.PromptTransformingChatService;
-import org.springframework.ai.tokenizer.JTokkitTokenCountEstimator;
-import org.springframework.ai.tokenizer.TokenCountEstimator;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,22 +15,17 @@ import org.springframework.context.annotation.Configuration;
 public class ChatConfig {
 
     @Bean
-    ChatMemory chatHistory() {
+    ChatMemory chatMemory() {
         return new InMemoryChatMemory();
     }
 
     @Bean
-    TokenCountEstimator tokenCountEstimator() {
-        return new JTokkitTokenCountEstimator();
-    }
-
-    @Bean
-    ChatService chatService(ChatModel chatModel, ChatMemory chatMemory, TokenCountEstimator tokenCountEstimator) {
-        return PromptTransformingChatService.builder(chatModel)
-                .withRetrievers(List.of(new ChatMemoryRetriever(chatMemory)))
-                .withContentPostProcessors(List.of(new LastMaxTokenSizeContentTransformer(tokenCountEstimator, 1000)))
-                .withAugmentors(List.of(new SystemPromptChatMemoryAugmentor()))
-                .withChatServiceListeners(List.of(new ChatMemoryChatServiceListener(chatMemory)))
+    ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory, VectorStore vectorStore) {
+        return chatClientBuilder
+                .defaultAdvisors(
+                        new MessageChatMemoryAdvisor(chatMemory),
+                        new QuestionAnswerAdvisor(vectorStore, SearchRequest.defaults()), // RAG
+                        new VectorStoreChatMemoryAdvisor(vectorStore))
                 .build();
     }
 }
--- chatbot/chatbot-openai-springai/src/test/java/com/example/chatbot/TestChatbotOpenaiApplication.java
+++ chatbot/chatbot-openai-springai/src/test/java/com/example/chatbot/TestChatbotOpenaiApplication.java
@@ -2,9 +2,19 @@ package com.example.chatbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestChatbotOpenaiApplication {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> pgvectorContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("pgvector/pgvector:pg16"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.from(ChatbotOpenaiApplication::main)
