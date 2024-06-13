66d9e08b5990afaa7c0b46474e10cb7f584cd001
Raja Kolli <rajadilipkolli@gmail.com>
2024-06-13 12:52:15 +0000

    feat : upgrade to milestone 1.0.0-M1

--- rag/rag-springai-ollama-llm/src/main/java/com/learning/ai/llmragwithspringai/service/AIChatService.java
+++ rag/rag-springai-ollama-llm/src/main/java/com/learning/ai/llmragwithspringai/service/AIChatService.java
@@ -1,18 +1,12 @@
 package com.learning.ai.llmragwithspringai.service;
 
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.messages.Message;
-import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.model.Generation;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.chat.prompt.SystemPromptTemplate;
-import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Service;
 
@@ -33,28 +27,16 @@ public class AIChatService {
             """;
 
     private final ChatClient aiClient;
-    private final VectorStore vectorStore;
 
     public AIChatService(ChatClient.Builder builder, VectorStore vectorStore) {
-        this.aiClient = builder.build();
-        this.vectorStore = vectorStore;
+        this.aiClient = builder.defaultSystem(template)
+                .defaultAdvisors(new QuestionAnswerAdvisor(
+                        vectorStore, SearchRequest.defaults().withTopK(5))) // RAG
+                .build();
     }
 
     public String chat(String query) {
-        // Querying the VectorStore using natural language looking for the information about info asked.
-        LOGGER.debug("Querying vector store with query :{}", query);
-        List<Document> listOfSimilarDocuments = this.vectorStore.similaritySearch(query);
-        String documents = listOfSimilarDocuments.stream()
-                .map(Document::getContent)
-                .collect(Collectors.joining(System.lineSeparator()));
-        LOGGER.info("Response from vector store :{}", documents);
-        // Constructing the systemMessage to indicate the AI model to use the passed information
-        // to answer the question.
-        Message systemMessage = new SystemPromptTemplate(template).createMessage(Map.of("documents", documents));
-        UserMessage userMessage = new UserMessage(query);
-        Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
-        LOGGER.info("Calling ai with prompt :{}", prompt);
-        ChatResponse aiResponse = aiClient.prompt(prompt).call().chatResponse();
+        ChatResponse aiResponse = aiClient.prompt().user(query).call().chatResponse();
         LOGGER.info("Response received from call :{}", aiResponse);
         Generation generation = aiResponse.getResult();
         return (generation != null) ? generation.getOutput().getContent() : "";
