cbe0c010957549076853f1d181b19e23e586c171
Raja Kolli <rajadilipkolli@gmail.com>
2024-12-24 14:04:56 +0000

    upgrade docker

--- rag/rag-springai-ollama-llm/src/main/java/com/learning/ai/llmragwithspringai/service/AIChatService.java
+++ rag/rag-springai-ollama-llm/src/main/java/com/learning/ai/llmragwithspringai/service/AIChatService.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.RetrievalAugmentationAdvisor;
-import org.springframework.ai.chat.prompt.AssistantPromptTemplate;
 import org.springframework.ai.rag.generation.augmentation.ContextualQueryAugmenter;
 import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
 import org.springframework.ai.vectorstore.VectorStore;
@@ -15,17 +14,6 @@ public class AIChatService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AIChatService.class);
 
-    private static final String template =
-            """
-            You are a helpful assistant, conversing with a user about the subjects contained in a set of documents.
-            Use the information from the DOCUMENTS section to provide accurate answers. If unsure or if the answer
-            isn't found in the DOCUMENTS section, simply state that you don't know the answer.
-
-            DOCUMENTS:
-            {query}
-
-            """;
-
     private final ChatClient aiClient;
 
     public AIChatService(ChatClient.Builder builder, VectorStore vectorStore) {
@@ -35,10 +23,8 @@ public class AIChatService {
                 .similarityThreshold(0.50)
                 .build();
 
-        var queryAugmenter = ContextualQueryAugmenter.builder()
-                .promptTemplate(new AssistantPromptTemplate(template))
-                .allowEmptyContext(true)
-                .build();
+        var queryAugmenter =
+                ContextualQueryAugmenter.builder().allowEmptyContext(true).build();
 
         RetrievalAugmentationAdvisor retrievalAugmentationAdvisor = RetrievalAugmentationAdvisor.builder()
                 .documentRetriever(documentRetriever)
