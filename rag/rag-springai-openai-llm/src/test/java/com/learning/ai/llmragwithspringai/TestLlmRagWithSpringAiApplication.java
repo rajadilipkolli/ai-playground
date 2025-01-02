aa7f8c0052482eaf92efaa902cd39a9fbfd79b2b
Raja Kolli <rajadilipkolli@gmail.com>
2025-01-02 08:54:19 +0000

    polish openai RAG

--- rag/rag-springai-openai-llm/src/test/java/com/learning/ai/llmragwithspringai/TestLlmRagWithSpringAiApplication.java
+++ rag/rag-springai-openai-llm/src/test/java/com/learning/ai/llmragwithspringai/TestLlmRagWithSpringAiApplication.java
@@ -1,24 +1,13 @@
 package com.learning.ai.llmragwithspringai;
 
+import com.learning.ai.llmragwithspringai.config.ContainersConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.utility.DockerImageName;
 
-@TestConfiguration(proxyBeanMethods = false)
 public class TestLlmRagWithSpringAiApplication {
-
-    @Bean
-    @ServiceConnection
-    PostgreSQLContainer<?> pgvectorContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("pgvector/pgvector:pg17"));
-    }
 
     public static void main(String[] args) {
         SpringApplication.from(LlmRagWithSpringAiApplication::main)
-                .with(TestLlmRagWithSpringAiApplication.class)
+                .with(ContainersConfig.class)
                 .run(args);
     }
 }
