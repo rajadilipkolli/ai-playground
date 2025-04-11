68a12b807130fd8e8a72b5ef634bd7a2eee88739
Raja Kolli <rajadilipkolli@gmail.com>
2025-04-11 21:47:04 +0000

    fix : compilation issue

--- chatbot/chatbot-ollama-springai/src/main/java/com/example/chatbot/config/ChatConfig.java
+++ chatbot/chatbot-ollama-springai/src/main/java/com/example/chatbot/config/ChatConfig.java
@@ -2,11 +2,8 @@ package com.example.chatbot.config;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
-import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
-import org.springframework.ai.chat.client.advisor.VectorStoreChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
-import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,13 +16,10 @@ public class ChatConfig {
     }
 
     @Bean
-    ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory, VectorStore vectorStore) {
+    ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
         return chatClientBuilder
                 .clone()
-                .defaultAdvisors(
-                        new MessageChatMemoryAdvisor(chatMemory),
-                        new QuestionAnswerAdvisor(vectorStore), // RAG
-                        new VectorStoreChatMemoryAdvisor(vectorStore))
+                .defaultAdvisors(new MessageChatMemoryAdvisor(chatMemory))
                 .build();
     }
 }
