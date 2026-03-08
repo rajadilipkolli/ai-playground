31f6ad2208bdb3a57814fd23942c3971820e918b
Raja Kolli <rajadilipkolli@gmail.com>
2026-03-08 09:16:34 +0000

    upgrade to latest version

--- embeddingstores/neo4j-springai/src/main/java/com/learning/ai/config/ResponseHeadersModification.java
+++ embeddingstores/neo4j-springai/src/main/java/com/learning/ai/config/ResponseHeadersModification.java
@@ -2,7 +2,6 @@ package com.learning.ai.config;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Collections;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +9,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClient;
 
 @Configuration(proxyBeanMethods = false)
@@ -33,9 +30,9 @@ class ResponseHeadersModification {
 
         public CustomClientHttpResponse(ClientHttpResponse originalResponse) {
             this.originalResponse = originalResponse;
-            MultiValueMap<String, String> modifiedHeaders = new LinkedMultiValueMap<>(originalResponse.getHeaders());
-            modifiedHeaders.put(HttpHeaders.CONTENT_TYPE, Collections.singletonList(MediaType.APPLICATION_JSON_VALUE));
-            this.headers = new HttpHeaders(modifiedHeaders);
+            HttpHeaders modifiedHeaders = new HttpHeaders(originalResponse.getHeaders());
+            modifiedHeaders.setContentType(MediaType.APPLICATION_JSON);
+            this.headers = modifiedHeaders;
         }
 
         @Override
