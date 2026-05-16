256327f9243d703f6864d233bd00ce3348e2c3b4
Raja Kolli <rajadilipkolli@gmail.com>
2026-05-16 13:14:20 +0000

    upgrade to spring boot 4

--- embeddingstores/pgvector-langchain4j/src/test/java/com/learning/ai/config/AbstractIntegrationTest.java
+++ embeddingstores/pgvector-langchain4j/src/test/java/com/learning/ai/config/AbstractIntegrationTest.java
@@ -2,10 +2,10 @@ package com.learning.ai.config;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 import com.learning.ai.TestPgVectorEmbeddingStoreExample;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
