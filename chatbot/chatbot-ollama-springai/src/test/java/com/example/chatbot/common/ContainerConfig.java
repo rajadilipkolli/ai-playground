54996db74f6fab78e18854945b5303615b8d17ff
Raja Kolli <rajadilipkolli@gmail.com>
2026-05-16 17:28:19 +0530

    upgrade in tests to use same version

--- chatbot/chatbot-ollama-springai/src/test/java/com/example/chatbot/common/ContainerConfig.java
+++ chatbot/chatbot-ollama-springai/src/test/java/com/example/chatbot/common/ContainerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.grafana.LgtmStackContainer;
 import org.testcontainers.ollama.OllamaContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -22,5 +23,12 @@ public class ContainerConfig {
     @ServiceConnection
     PostgreSQLContainer postgreSQLContainer() {
         return new PostgreSQLContainer<>(DockerImageName.parse("pgvector/pgvector:pg17"));
+    }
+
+    @Bean
+    @ServiceConnection
+    LgtmStackContainer lgtmStackContainer() {
+        return new LgtmStackContainer(DockerImageName.parse("grafana/otel-lgtm").withTag("0.28.0"))
+                .withStartupTimeout(Duration.ofMinutes(2));
     }
 }
